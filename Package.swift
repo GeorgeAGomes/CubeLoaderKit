6c231b69d60cc68b85ccea7074244ff9a383b4e1
George <georgevag@protonmail.com>
2025-04-11 20:38:52 -0300

    Add LUT converter

--- Package.swift
+++ Package.swift
@@ -4,21 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "CubeLoaderKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CubeLoaderKit",
-            targets: ["CubeLoaderKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CubeLoaderKit"),
-        .testTarget(
-            name: "CubeLoaderKitTests",
-            dependencies: ["CubeLoaderKit"]
-        ),
-    ]
+	name: "CubeLoaderKit",
+	platforms: [
+		.iOS(.v13)
+	],
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "CubeLoaderKit",
+			targets: ["CubeLoaderKit"]),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "CubeLoaderKit"),
+		.testTarget(
+			name: "CubeLoaderKitTests",
+			dependencies: ["CubeLoaderKit"]
+		),
+	]
 )
